faa9c552e7afad6af319a8101eeb4ae7df88ef02
Owen Kieffer-Jones <owen.kieffer-jones@outletcity.com>
2024-01-21 12:58:40 +0100

    fix light-mode hero

--- components/content/Hero.vue
+++ components/content/Hero.vue
@@ -19,19 +19,23 @@ withDefaults(
       :width="1216"
       :placeholder="[100, 50, 20]"
     />
-    <div class="absolute w-full h-full -z-1 bg-gray-950/70" />
+    <div
+      class="absolute w-full h-full -z-1 bg-gray-700/50 dark:bg-gray-950/70"
+    />
     <div class="py-12 md:py-20 px-4 sm:px-6 relative">
-      <div class="max-w-2xl mx-auto prose">
-        <LogoLarge class="h-56 mx-auto text-primary-300" />
-        <div class="mt-20">
-          <div class="text-xl sm:text-2xl font-extrabold mb-4 text-center">
-            <slot name="title" />
-          </div>
-          <div
-            class="text-lg text-subtle p-4 backdrop-blur rounded-3xl bg-gray-500/20 px-8"
-          >
-            <slot name="description" />
-          </div>
+      <div class="max-w-2xl mx-auto prose text-gray-100">
+        <LogoLarge
+          class="h-56 mx-auto text-primary-400 drop-shadow-md dark:text-primary-300 mb-20"
+        />
+        <div
+          class="text-xl sm:text-2xl font-extrabold mb-4 text-center drop-shadow"
+        >
+          <slot name="title" />
+        </div>
+        <div
+          class="text-lg dark:text-gray-300 p-4 backdrop-blur rounded-3xl bg-gray-500/20 px-8"
+        >
+          <slot name="description" />
         </div>
       </div>
     </div>
