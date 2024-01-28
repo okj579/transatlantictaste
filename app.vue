e09d85731801e425844f94c70da8078e9e3babeb
Owen Kieffer-Jones <owen.kieffer-jones@outletcity.com>
2024-01-28 19:10:00 +0100

    fix canonical

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 const { t } = useI18n();
-useHead(useLocaleHead({ addSeoAttributes: true }));
 useHead(() => ({
   link: [{ rel: "icon", href: "/favicon.png" }],
   meta: [
